dbde865fd704f5f1032028ef0ea137f01ac5e183
Tsoler Hayitian <tsoler.hayitian@gmail.com>
2025-09-23 10:27:35 +0200

    class DiaryEntry

---
+++ DagsBokApp/Domänklass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DagsBokApp
+{
+    public class DiaryEntry
+    {
+        public DateTime Date { get; set; }
+        public string Text { get; set; }
+
+     public override string ToString()
+        {
+            return $"{Date.ToShortDateString()}: {Text}";
+        }
+    }
+}
