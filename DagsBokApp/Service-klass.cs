0ab113d3f1cd8ff6594254a15930ae947f390eab
Tsoler Hayitian <tsoler.hayitian@gmail.com>
2025-09-25 10:15:25 +0200

    Class DiaryServis

---
+++ DagsBokApp/Service-klass.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+namespace DagsBokApp
+{
+
+    public class DiaryService
+    {
+        private readonly string filePath;
+        private readonly string errorLogPath = "errorlog.txt";
+
+        public DiaryService(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public void Save(List<DiaryEntry> entries)
+        {
+            try
+
+        }
+
+        {
+           
+    }
+}
